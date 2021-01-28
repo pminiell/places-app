19919d521401062575c58ede2e16db4d5a3d0c7b
Philip <minielly5@hotmail.com>
2021-01-28 13:29:03 -0500

    started building the add place form with validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,40 @@
-import { React } from "react";
+import React, { useCallback } from "react";
 
-const NewPlace = () =>{
-    return(
-        <h2>Places Works!</h2>
-    )
-}
+import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/utils/validators";
+import "./NewPlace.css";
+
+const NewPlace = () => {
+    const titleInputHandler = useCallback((id, value, isValid) =>{
+
+    }, []);
+
+    const descriptionInputHandler = useCallback((id, value, isValid) =>{
+
+    }, []);
+    
+
+    return (
+    <form className="place-form">
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+        onInput = {titleInputHandler}
+      />
+       <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput = {descriptionInputHandler}
+      />
+    </form>
+  );
+};
 
 export default NewPlace;
