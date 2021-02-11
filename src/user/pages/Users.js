b664779624e3a23903a8d50556ba20c3dec48181
Philip <minielly5@hotmail.com>
2021-02-10 23:16:13 -0500

    aaaaaaaaaaaaaaa

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -12,7 +12,7 @@ const Users = () => {
     const fetchUsers = async () => {
       try {
         const responseData = await sendRequest(
-          "http://localhost:5000/api/users/"
+          `${process.env.REACT_APP_BACKEND_URL}/api/users/`
         );
         console.log(responseData);
         setLoadedUsers(responseData.users);
