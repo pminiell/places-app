b5ab7722522c687ff962fc251cb68cc7ce53fdeb
Philip <minielly5@hotmail.com>
2021-02-23 22:51:38 -0500

    test

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -14,7 +14,6 @@ const Users = () => {
         const responseData = await sendRequest(
           `${process.env.REACT_APP_BACKEND_URL}/api/users/`
         );
-        console.log(responseData);
         setLoadedUsers(responseData.users);
       } catch (err) {}
     };
